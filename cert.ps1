4a4f908d5fef061b4057dd91f85c0aa427bfea47
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-10-07 21:38:43 +0800

    Add cert.ps1 and update packaging process
    Moved certificate creation logic from package.ps1 to a new cert.ps1 script. Updated package.ps1 to use a hardcoded certificate thumbprint. Added ipatool.exe to IPAbuyer.csproj output for packaging.

---
+++ cert.ps1
@@ -0,0 +1,13 @@
+$cert = New-SelfSignedCertificate `
+    -Type Custom `
+    -Subject "CN=IPAbuyer Publisher" `
+    -KeyUsage DigitalSignature `
+    -FriendlyName "IPAbuyer Signing Certificate" `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") `
+    -NotAfter (Get-Date).AddYears(2) `
+    -KeyExportPolicy Exportable
+
+$thumbprint = $cert.Thumbprint
+
+echo "$thumbprint"
--- package.ps1
+++ package.ps1
@@ -1,14 +1,4 @@
-$cert = New-SelfSignedCertificate `
-    -Type Custom `
-    -Subject "CN=IPAbuyer Publisher" `
-    -KeyUsage DigitalSignature `
-    -FriendlyName "IPAbuyer Signing Certificate" `
-    -CertStoreLocation "Cert:\CurrentUser\My" `
-    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") `
-    -NotAfter (Get-Date).AddYears(2) `
-    -KeyExportPolicy Exportable
-
-$thumbprint = $cert.Thumbprint
+$thumbprint = "5112B19D4500CFF97AE01A9DE4DB81F447EC22EA"
 
 dotnet build .\src\IPAbuyer.csproj `
 -c Release /p:WindowsPackageType=MSIX `
