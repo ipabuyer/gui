6898daeee53c787d441dca4252d2914fda73b66b
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2026-01-05 10:57:33 +0800

    重构数据库路径设置逻辑，简化变量使用

--- src/Models/Database.cs
+++ src/Models/Database.cs
@@ -6,20 +6,26 @@ namespace IPAbuyer.Models
 {
     public class Database
     {
-        public string? packagepath;
-        public string? appdatapath;
-        public string? AppDBname = "PurchasedAppDb.db";
-        public string? AccountDBname = "KeychainConfig.db";
-        public string? AppDB;
-        public string? AccountDB;
+
+        public string? path; // 文件根目录
+        public string? AppDBname = "PurchasedAppDb.db"; // 已购买app数据库文件名
+        public string? AccountDBname = "KeychainConfig.db"; // 账户数据库文件名
+        public string? AppDB; // 已购买app数据库路径
+        public string? AccountDB; // 账户数据库路径
 
         public Database()
         {
+            // 设置数据库路径
+
+#if RELEASE // 生产版本
             try
             {
-                packagepath = ApplicationData.Current.LocalFolder.Path;
-                AppDB = Path.Combine(packagepath, AppDBname);
-                AccountDB = Path.Combine(packagepath, AccountDBname);
+                // 获取安装目录
+                path = ApplicationData.Current.LocalFolder.Path;
+                AppDB = Path.Combine(path, AppDBname);
+                AccountDB = Path.Combine(path, AccountDBname);
+                
+                // 如果数据库文件不存在则创建
                 if (!File.Exists(AppDB))
                 {
                     File.Create(AppDB).Close();
@@ -32,11 +38,20 @@ namespace IPAbuyer.Models
             catch(Exception ex)
             {
                 Debug.WriteLine($"数据库错误: {ex.Message}");
-                appdatapath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "IPAbuyer");
-                if (!Directory.Exists(appdatapath))
-                    Directory.CreateDirectory(appdatapath);
-                AppDB = Path.Combine(appdatapath, AppDBname);
-                AccountDB = Path.Combine(appdatapath, AccountDBname);
+            }
+#elif DEBUG // 调试版本
+            try
+            {
+                // 使用appdata文件夹
+                path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "IPAbuyer");
+
+                // 创建appdata内的目录
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                AppDB = Path.Combine(path, AppDBname);
+                AccountDB = Path.Combine(path, AccountDBname);
+
+                // 如果数据库文件不存在则创建
                 if (!File.Exists(AppDB))
                 {
                     File.Create(AppDB).Close();
@@ -46,6 +61,11 @@ namespace IPAbuyer.Models
                     File.Create(AccountDB).Close();
                 }
             }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"数据库错误: {ex.Message}");
+            }
+#endif
         }
     }
 }
